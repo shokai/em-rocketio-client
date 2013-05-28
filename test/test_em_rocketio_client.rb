8bd980ce2bbca3e31e103647f043d35a1c36064b
Sho Hashimoto <hashimoto@shokai.org>
2013-05-29 05:47:48 +0900

    MiniTest::Test

--- test/test_em_rocketio_client.rb
+++ test/test_em_rocketio_client.rb
@@ -1,6 +1,6 @@
 require File.expand_path 'test_helper', File.dirname(__FILE__)
 
-class TestEmRocketioClient < MiniTest::Unit::TestCase
+class TestEmRocketioClient < MiniTest::Test
 
   def test_websocket_to_comet
     ## websocket --> server --> comet
